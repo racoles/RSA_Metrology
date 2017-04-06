13610e5c95d86c850698fb23f968c40a84e12fc0
shouraku <shouraku@gmail.com>
2017-04-06 13:18:15 -0400

    GUI button update

--- RSAMetGUI.py
+++ RSAMetGUI.py
@@ -34,30 +34,30 @@ class RSAMetGUI(object):
         S00 = Button(master, text="S00", width=10, command=self.loadInputDataFile)
         S00.grid(row=2, column=2)
 
-        S01 = Button(master, text="S01", width=10, command=self.openFile)
+        S01 = Button(master, text="S01", width=10, command=self.loadInputDataFile)
         S01.grid(row=1, column=2)
 
-        S02 = Button(master, text="S02", width=10, command=self.openFile)
+        S02 = Button(master, text="S02", width=10, command=self.loadInputDataFile)
         S02.grid(row=0, column=2)
         
         #REB1
-        S10 = Button(master, text="S10", width=10, command=self.openFile)
+        S10 = Button(master, text="S10", width=10, command=self.loadInputDataFile)
         S10.grid(row=2, column=1)
 
-        S11 = Button(master, text="S11", width=10, command=self.openFile)
+        S11 = Button(master, text="S11", width=10, command=self.loadInputDataFile)
         S11.grid(row=1, column=1)
 
-        S12 = Button(master, text="S12", width=10, command=self.openFile)
+        S12 = Button(master, text="S12", width=10, command=self.loadInputDataFile)
         S12.grid(row=0, column=1)
         
         #REB2
-        S20 = Button(master, text="S20", width=10, command=self.openFile)
+        S20 = Button(master, text="S20", width=10, command=self.loadInputDataFile)
         S20.grid(row=2, column=0)
 
-        S21 = Button(master, text="S21", width=10, command=self.openFile)
+        S21 = Button(master, text="S21", width=10, command=self.loadInputDataFile)
         S21.grid(row=1, column=0)
 
-        S22 = Button(master, text="S22", width=10, command=self.openFile)
+        S22 = Button(master, text="S22", width=10, command=self.loadInputDataFile)
         S22.grid(row=0, column=0)
         
     def loadInputDataFile(self):
