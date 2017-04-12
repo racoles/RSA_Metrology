7c7270cebcb4612eb4d5090800fcfbcf1ac3ff0a
shouraku <shouraku@gmail.com>
2017-04-12 09:40:36 -0400

    Update button text

--- RSAMetGUI.py
+++ RSAMetGUI.py
@@ -34,30 +34,30 @@ class RSAMetGUI(object):
         S00 = Button(master, text="S00", width=10, command=lambda:self.loadInputDataFile(S00))
         S00.grid(row=2, column=2)
 
-        S01 = Button(master, text="S01", width=10, command=self.loadInputDataFile)
+        S01 = Button(master, text="S01", width=10, command=lambda:self.loadInputDataFile(S01))
         S01.grid(row=1, column=2)
 
-        S02 = Button(master, text="S02", width=10, command=self.loadInputDataFile)
+        S02 = Button(master, text="S02", width=10, command=lambda:self.loadInputDataFile(S02))
         S02.grid(row=0, column=2)
         
         #REB1
-        S10 = Button(master, text="S10", width=10, command=self.loadInputDataFile)
+        S10 = Button(master, text="S10", width=10, command=lambda:self.loadInputDataFile(S10))
         S10.grid(row=2, column=1)
 
-        S11 = Button(master, text="S11", width=10, command=self.loadInputDataFile)
+        S11 = Button(master, text="S11", width=10, command=lambda:self.loadInputDataFile(S11))
         S11.grid(row=1, column=1)
 
-        S12 = Button(master, text="S12", width=10, command=self.loadInputDataFile)
+        S12 = Button(master, text="S12", width=10, command=lambda:self.loadInputDataFile(S12))
         S12.grid(row=0, column=1)
         
         #REB2
-        S20 = Button(master, text="S20", width=10, command=self.loadInputDataFile)
+        S20 = Button(master, text="S20", width=10, command=lambda:self.loadInputDataFile(S20))
         S20.grid(row=2, column=0)
 
-        S21 = Button(master, text="S21", width=10, command=self.loadInputDataFile)
+        S21 = Button(master, text="S21", width=10, command=lambda:self.loadInputDataFile(S21))
         S21.grid(row=1, column=0)
 
-        S22 = Button(master, text="S22", width=10, command=self.loadInputDataFile)
+        S22 = Button(master, text="S22", width=10, command=lambda:self.loadInputDataFile(S22))
         S22.grid(row=0, column=0)
         
         #Add coordinate compass
@@ -70,11 +70,16 @@ class RSAMetGUI(object):
         '''
         Load metrology data from input file using openpyxl library
         '''
+        #open file
         fileName = self.openFile()
+        #load workbook
         self.wb = load_workbook(fileName, read_only=True)
+        #load Sheet1
         self.ws = self.wb.get_sheet_by_name('Sheet1')
+        #create a python list of the data by iterating over all of the Sheet1 data
         pythonList = list(self.iter_rows(self.ws))
         print(pythonList)
+        #update the button text to show the filename
         sensorButton.config(text = fileName)
 
         
