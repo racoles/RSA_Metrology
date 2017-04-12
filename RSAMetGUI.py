7bbb89457adb0f2fb0ac9149880dea8b20a8b34c
shouraku <shouraku@gmail.com>
2017-04-12 14:43:29 -0400

    Update button text

--- RSAMetGUI.py
+++ RSAMetGUI.py
@@ -32,33 +32,33 @@ class RSAMetGUI(object):
         #                <--+X    Ø +Z
         
         #REB0
-        S00 = Button(master, text="S00", width=10, command=lambda:self.loadInputDataFile(S00))
+        S00 = Button(master, text="S00", command=lambda:self.loadInputDataFile(S00))
         S00.grid(row=2, column=2)
-
-        S01 = Button(master, text="S01", width=10, command=lambda:self.loadInputDataFile(S01))
+        
+        S01 = Button(master, text="S01", command=lambda:self.loadInputDataFile(S01))
         S01.grid(row=1, column=2)
 
-        S02 = Button(master, text="S02", width=10, command=lambda:self.loadInputDataFile(S02))
+        S02 = Button(master, text="S02", command=lambda:self.loadInputDataFile(S02))
         S02.grid(row=0, column=2)
         
         #REB1
-        S10 = Button(master, text="S10", width=10, command=lambda:self.loadInputDataFile(S10))
+        S10 = Button(master, text="S10", command=lambda:self.loadInputDataFile(S10))
         S10.grid(row=2, column=1)
 
-        S11 = Button(master, text="S11", width=10, command=lambda:self.loadInputDataFile(S11))
+        S11 = Button(master, text="S11", command=lambda:self.loadInputDataFile(S11))
         S11.grid(row=1, column=1)
 
-        S12 = Button(master, text="S12", width=10, command=lambda:self.loadInputDataFile(S12))
+        S12 = Button(master, text="S12", command=lambda:self.loadInputDataFile(S12))
         S12.grid(row=0, column=1)
         
         #REB2
-        S20 = Button(master, text="S20", width=10, command=lambda:self.loadInputDataFile(S20))
+        S20 = Button(master, text="S20", command=lambda:self.loadInputDataFile(S20))
         S20.grid(row=2, column=0)
 
-        S21 = Button(master, text="S21", width=10, command=lambda:self.loadInputDataFile(S21))
+        S21 = Button(master, text="S21", command=lambda:self.loadInputDataFile(S21))
         S21.grid(row=1, column=0)
 
-        S22 = Button(master, text="S22", width=10, command=lambda:self.loadInputDataFile(S22))
+        S22 = Button(master, text="S22", command=lambda:self.loadInputDataFile(S22))
         S22.grid(row=0, column=0)
         
         #Add coordinate compass
