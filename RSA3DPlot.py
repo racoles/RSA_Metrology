92f0368e5c225029083d3adc845758a4331c41b0
shouraku <shouraku@gmail.com>
2017-04-24 17:03:47 -0400

    Sensor placement on RSA

--- RSA3DPlot.py
+++ RSA3DPlot.py
@@ -6,7 +6,7 @@ Created on Apr 14, 2017
 '''
 
 # Import #######################################################################################
-from numpy import array, rot90, full, concatenate, zeros
+from numpy import array, full, concatenate, copy, empty
 ################################################################################################
 
 class RSA3DPlot(object):
@@ -19,20 +19,34 @@ class RSA3DPlot(object):
         '''
         Constructor
         '''
-    
-    def convertToNumpy(self, RSAList):
-        '''
-        Convert python list to numpy array
-        '''
-        return array(RSAList)
         
-    def coordTransform(self, SensorArray):
+    def coordTransform(self, sensorPCS, Ymax):
         '''
         Perform coordinate transform from PCS coordinate system to CCS coordinate system (180 degree rotation).
+        CCS --->+X
+         |   __________
+         +Y |        o |
+            |          |
+            |          |  
+         +Y |          |
+         |  |_o______o_|
+        PCS --->+X
+        
+        X(CCS) = X(PCS)
+        Y(CCS) = Y(PCS)MAX - Y(PCS)
         '''
-        return rot90(SensorArray,2)
+        #Coordinate transform Y values
+        yValues = copy(sensorPCS[:,1])
+        yValues = [Ymax-yy for yy in yValues]
+        print(yValues)
+        #Insert CCS Y coordinates into array
+        sensorCCS = copy(sensorPCS)
+        sensorCCS[:,1] = yValues
+        return sensorCCS
+        
+        
     
-    def createVirtualRSA(self, S00, S01, S02, S10, S11, S12, S20, S21, S22):
+    def createVirtualRSA(self, S00List, S01List, S02List, S10List, S11List, S12List, S20List, S21List, S22List):
         '''
         Align data based on the sensor positions on the RSA.
         
@@ -43,6 +57,21 @@ class RSA3DPlot(object):
                                  
                         <--+X    Ø +Z
         '''
+        ###########################################################################
+        ###Convert from Python list to Numpy array
+        ###########################################################################
+        #REB0
+        S00 = array(S00List)
+        S01 = array(S01List)
+        S02 = array(S02List)
+        #REB1
+        S10 = array(S10List)
+        S11 = array(S11List)
+        S12 = array(S12List)
+        #REB2
+        S20 = array(S20List)
+        S21 = array(S21List)
+        S22 = array(S22List)
         ###########################################################################
         ###Add Color column for later plotting
         ###########################################################################
@@ -95,21 +124,16 @@ class RSA3DPlot(object):
         #S00
         S00XMax, S00YMax = S00.nanmax(axis=0)
         print(S00XMax, S00YMax)
-        #S01
         S01XMax, S01YMax = S01.nanmax(axis=0)
         print(S01XMax, S01YMax)
-        #S02
         S02XMax, S02YMax = S02.nanmax(axis=0)
         print(S02XMax, S02YMax)
         
         #REB1 Sensors
-        #S10
         S10XMax, S10YMax = S10.nanmax(axis=0)
         print(S10XMax, S10YMax)
-        #S11
         S11XMax, S11YMax = S11.nanmax(axis=0)
         print(S11XMax, S11YMax)
-        #S12
         S12XMax, S12YMax = S12.nanmax(axis=0)
         print(S12XMax, S12YMax)
         
@@ -117,29 +141,30 @@ class RSA3DPlot(object):
         #S20
         S20XMax, S20YMax = S20.nanmax(axis=0)
         print(S20XMax, S20YMax)
-        #S21
         S21XMax, S21YMax = S21.nanmax(axis=0)
         print(S21XMax, S21YMax)
-        #S22
         S22XMax, S22YMax = S22.nanmax(axis=0)
         print(S22XMax, S22YMax)
         
         ###########################################################################
-        ###Turn list into CCD shaped array
-        ###########################################################################   
-        
+        ###Rotate sensors (coordinate transform from PCS to CCS coordinate systems)
         ###########################################################################
-        ###Rotate sensors (coord transform from PCS to CCS coordinate systems)
-        ###########################################################################   
+        #REB0
+        S00CCS = self.coordTransform(S00, S00YMax)
+        S01CCS = self.coordTransform(S01, S01YMax)
+        S02CCS = self.coordTransform(S02, S02YMax)
+        #REB1
+        S10CCS = self.coordTransform(S10, S10YMax)
+        S11CCS = self.coordTransform(S11, S11YMax)
+        S12CCS = self.coordTransform(S12, S12YMax)
+        #REB2
+        S20CCS = self.coordTransform(S20, S20YMax)
+        S21CCS = self.coordTransform(S21, S21YMax)
+        S22CCS = self.coordTransform(S22, S22YMax)
         
         ###########################################################################
         ###Place sensors in RSA positions
         ###########################################################################
-        #Create RSA array
-        xx = S00.shape[0] + S01.shape[0] + S02.shape[0] + S10.shape[0] + S11.shape[0] + S12.shape[0] + S20.shape[0] + S21.shape[0] + S22.shape[0]
-        yy = S00.shape[1] + S01.shape[1] + S02.shape[1] + S10.shape[1] + S11.shape[1] + S12.shape[1] + S20.shape[1] + S21.shape[1] + S22.shape[1]
-        RSAarray = zeros(xx, yy)
-        
         #Place sensors: from left to right and top to bottom
         #Sensor location buttons to open data files for each sensor (CCS Coordinate system)
         #    S22    S12    S02
@@ -148,18 +173,13 @@ class RSA3DPlot(object):
         #                         
         #                <--+X    Ø +Z
         #This will be done by adding the appropriate value to each X and Y values 
-        #    so that the plot will show the data in the proper position in a plot
-        #S22
-        
-        #S12
-        #S02
-        #S21
-        #S11
-        #S01
-        #S20
-        #S10
+        #    so that the data will show the proper position in a plot
+        #
+        #Create numpy RSA array
+        RSAArray = empty(0,4)
         #S00
-        
+        concatenate((RSAArray, S00CCS))
+        #S10 (next to S00)
         
     def plotSensors3D(self):
         '''
